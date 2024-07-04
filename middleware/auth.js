5a2a6b52523dc594538ae761599935532eb37703
Kylee Sawyer <k.sawyer@ufl.edu>
2024-07-04 03:02:15 -0400

    deploying application

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,7 +1,11 @@
 function checkAuth(req, res, next) {
 	// Check req.session.loggedIn to see if the user is logged in
-	// If the user is logged in, call next()
+	if (req.session && req.session.loggedIn) {
+		// If the user is logged in, call next()
+		return next()
+	}
 	// If the user is not logged in, redirect to /login
+	res.redirect('/login')
 }
 
 module.exports = checkAuth
